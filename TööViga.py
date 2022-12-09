fe0c8d019a63b45abd6788f9220ad8409534a568
EdvardDatser <edvard.datser@icloud.com>
2022-12-09 15:13:47 +0200

    Add project files.

---
+++ TööViga.py
@@ -0,0 +1,34 @@
+from math import *
+
+print("Ruudu karakteristikud: ")
+a=float(input('Sisesta ruudu külje pikkus => '))
+S=a**2
+print("Ruudu pindala", S)
+P=4*a
+print("Ruudu ümbermõõt", P)
+di=a*sqrt(2)
+print("Ruudu diagonaal", round(di,2))
+print()
+print("Ristküliku karakteristikud")
+b=float(input("Sisesta ristküliku 1. külje pikkus => "))
+c=float(input("Sisesta ristküliku 2. külje pikkus => "))
+S=b*c
+print( "Ristküliku pindala", S)
+P=2*(b+c)
+print("Ristküliku ümbermõõt", P)
+di=sqrt(b*2+c*2)
+print("Ristküliku diagonaal", round(di,2))
+print()
+print("Ringi karakteristikud")
+r=float(input( "Sisesta ringi raadiusi pikkus => " ))
+d=2*r
+print("Ringi läbimõõt", d)
+S=pi*r**2
+print("Ringi pindala", round(S,2))
+C=2*pi*r
+print("Ringjoone pikkus", round(C,2))
+
+
+#Прежде всего я исправил то как импортировали математические функции.
+#Далее большинство формул были записаны некорректно.
+#И так как питон строго типированный язык программирования не было от слова совсем расставлены форматы строк.
